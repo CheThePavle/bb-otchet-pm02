f7e3a63fa4d0530eee84c09f4c0b3131dd83a5d4
Chikurov Pavel <84593835+CheThePavle@users.noreply.github.com>
2022-08-05 10:46:37 +0400

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,11 @@
-import { useEffect, useRef } from 'react';
-import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react';
+import Head from 'next/head';
+import { Image } from 'antd'
 
 export default function Test() {
   const ClientCamera = useRef();
   const ManagerVoice = useRef();
+  const [img, setImg] = useState(null);
 
   let currentCamera = "back";
   let camera = {
@@ -14,8 +16,8 @@ export default function Test() {
   useEffect(() => {
     let forPhoto = {
       video: {
-        width: { ideal: 640 },
-        height: { ideal: 360 }
+        width: { ideal: 1980 },
+        height: { ideal: 1080 }
       },
     };
 
@@ -31,10 +33,40 @@ export default function Test() {
     .catch(alert);
   });
 
+  const makePhoto = () => {
+    let video = ClientCamera.current;
+    let width = video.offsetWidth;
+    let height = video.offsetHeight;
+    let newWidth = 0;
+    let newHeight = 0;
+
+    while (newWidth < 3000 && newHeight < 3000) {
+      newWidth += width;
+      newHeight += height;
+    }
+    const res = {
+      width: newWidth,
+      height: newHeight
+    };
+
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    canvas.width = res.width;
+    canvas.height = res.height;
+    context.drawImage(video, 0, 0, res.width, res.height);
+
+    let data = canvas.toDataURL("image/jpeg", 0.5);  
+    setImg(data);  
+  }
+
   return <>
     <Head>
       <meta name="viewport" content={"width=device-width"} />
     </Head>
+    <div>
+      <Image style={{ width: '100px', height: '100px', objectFit: 'cover' }} src={img} />
+      <span tabIndex={0} role="button" style={{ color: '#D02323', textDecorationLine: 'underline', cursor: 'pointer', fontSize: '2em' }} onClick={() => makePhoto()}>Сделать снимок</span>
+    </div>
     <div>
       <video
         muted
